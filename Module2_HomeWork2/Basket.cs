6bde392a57d124e4f4f1ce8c5ba4ebee6715ed45
Danylo Sereda <daniildanila42@gmail.com>
2023-05-29 10:00:23 +0300

    Finish code

---
+++ Module2_HomeWork2/Basket.cs
@@ -0,0 +1,167 @@
+﻿public class Basket
+{
+    private Store _store;
+
+    public Basket(Store store)
+    {
+        _store = store;
+        BasketItems = new int?[10];
+        ItemCount = new int[10];
+    }
+
+    public int?[] BasketItems { get; private set; }
+    public int[] ItemCount { get; private set; }
+
+    public void SelectAndAddItems()
+    {
+        // Выбор и добавление товаров в корзину до заполнения
+        while (GetTotalItemCount() < 10)
+        {
+            Console.WriteLine("\nEnter the items you want to add to the basket (for example Bananas 2, Apples 3):");
+            string input = Console.ReadLine();
+
+            string[] items = input.Split(',');
+
+            int totalAdded = 0;
+            string[] lastItemName = null;
+            int lastItemQuantity = 0;
+
+            foreach (string item in items)
+            {
+                string[] parts = item.Trim().Split(' ');
+                if (parts.Length == 2 && int.TryParse(parts[1], out int quantity))
+                {
+                    int index = GetItemIndex(parts[0]);
+
+                    if (index >= 0 && _store.IsItemAvailable(index) && quantity > 0 && quantity <= _store.GetStockQuantity(index))
+                    {
+                        int remainingSpace = 10 - totalAdded;
+
+                        if (totalAdded + quantity > 10)
+                        {
+                            lastItemName = _store.GetStoreItems();
+                            lastItemQuantity = remainingSpace;
+                            Console.WriteLine($"Added only {lastItemQuantity} {lastItemName[index]} to the basket. Basket is full.");
+                            break;
+                        }
+
+                        AddItem(index, quantity);
+                        _store.SellItem(index, quantity);
+                        totalAdded += quantity;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"It's out of stock! {item.Trim()}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid item format! {item.Trim()}");
+                }
+            }
+
+            if (lastItemName != null && lastItemQuantity > 0)
+            {
+                int lastItemIndex = GetItemIndex(lastItemName[lastItemName.Length - 1]);
+                AddItem(lastItemIndex, lastItemQuantity);
+                _store.SellItem(lastItemIndex, lastItemQuantity);
+            }
+
+            if (GetTotalItemCount() >= 10)
+            {
+                Console.WriteLine("\nBasket is full. Order is complete.");
+                break;
+            }
+
+            Console.WriteLine("\nDo you want to add more items? (yes/no)");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() != "yes")
+            {
+                Console.WriteLine("\nOrder is complete.");
+                break;
+            }
+        }
+    }
+
+    public void AddItem(int index, int quantity)
+    {
+        int itemCount = GetTotalItemCount();
+
+        // Добавление товара в корзину с учетом доступного места
+        if (BasketItems[index] == null)
+        {
+            if (quantity + itemCount > 10)
+            {
+                quantity = 10 - itemCount;
+            }
+
+            BasketItems[index] = index; // Добавление индекса товара в корзину
+            ItemCount[index] = quantity; // Установка количества товара
+        }
+        else
+        {
+            int availableQuantity = 10 - itemCount;
+
+            if (quantity > availableQuantity)
+            {
+                quantity = availableQuantity;
+            }
+
+            ItemCount[index] += quantity; // Увеличение количества товара в корзине
+        }
+
+        Console.WriteLine($"Added: {quantity} {_store.GetStoreItems()[index]} to the basket.");
+    }
+
+    public void DisplayBasketItems()
+    {
+        Console.WriteLine("\nItems in the basket (Total ammount 10):\n");
+
+        bool isEmpty = true;
+
+        // Вывод выбранных товаров и их количества
+        for (int i = 0; i < BasketItems.Length; i++)
+        {
+            if (BasketItems[i] != null && ItemCount[i] > 0)
+            {
+                Console.WriteLine($"{_store.GetStoreItems()[i]} - quantity: {ItemCount[i]}");
+                isEmpty = false;
+            }
+        }
+
+        if (isEmpty)
+        {
+            Console.WriteLine("Basket is empty.");
+        }
+    }
+
+    private int GetTotalItemCount()
+    {
+        int totalCount = 0;
+
+        // Получение общего количества товаров в корзине
+        for (int i = 0; i < ItemCount.Length; i++)
+        {
+            totalCount += ItemCount[i];
+        }
+
+        return totalCount;
+    }
+
+    private int GetItemIndex(string itemName)
+    {
+        string[] storeItems = _store.GetStoreItems();
+
+        // Получение индекса товара по его названию
+        for (int i = 0; i < storeItems.Length; i++)
+        {
+            if (storeItems[i] == itemName)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Module2_HomeWork2/Store.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+public class Store
+{
+    private string[] _storeItems;
+    private int[] _inStock;
+    private int[] _prices;
+
+    public Store()
+    {
+        _storeItems = new string[]
+        {
+            "Bananas",
+            "Apples",
+            "Oranges",
+            "Penuts",
+            "Pineapples",
+            "Strawberry",
+            "Plums",
+            "Milk",
+            "Chocolate",
+            "Icecream"
+        };
+
+        _inStock = new int[10];
+        _prices = new int[10];
+
+        Random random = new Random();
+
+        // Заполнение массивов случайными значениями
+        for (int i = 0; i < _inStock.Length; i++)
+        {
+            _inStock[i] = random.Next(0, 20);
+            _prices[i] = random.Next(1, 100);
+        }
+    }
+
+    // Проверка, доступен ли товар с заданным индексом
+    public bool IsItemAvailable(int index)
+    {
+        return _inStock[index] > 0;
+    }
+
+    // Вычитание проданного количества товара из общего количества
+    public void SellItem(int index, int quantity)
+    {
+        if (_inStock[index] >= quantity)
+        {
+            _inStock[index] -= quantity;
+        }
+    }
+
+    // Возвращает массив названий товаров в магазине
+    public string[] GetStoreItems()
+    {
+        return _storeItems;
+    }
+
+    // Возвращает количество товара с заданным индексом в наличии
+    public int GetStockQuantity(int index)
+    {
+        return _inStock[index];
+    }
+
+    // Возвращает цену товара с заданным индексом
+    public int GetPriceForItem(int index)
+    {
+        return _prices[index];
+    }
+
+    public void DisplayStock()
+    {
+        Console.WriteLine("Currently in stock:\n");
+        bool allInStock = true;
+
+        // Вывод товаров, которые есть в наличии, с их количеством и ценой
+        for (int i = 0; i < _inStock.Length; i++)
+        {
+            int amount = _inStock[i];
+            int price = _prices[i];
+            if (amount > 0)
+            {
+                Console.WriteLine($"{_storeItems[i]} - quantity: {amount} price: {price} UAH");
+                allInStock = false;
+            }
+        }
+
+        if (allInStock)
+        {
+            Console.WriteLine("Everything in stock.");
+        }
+
+        Console.WriteLine("\nOut of stock:\n");
+        bool outOfStockExists = false;
+
+        // Вывод товаров, которых нет в наличии
+        for (int i = 0; i < _inStock.Length; i++)
+        {
+            int amount = _inStock[i];
+            if (amount == 0)
+            {
+                Console.WriteLine($"{_storeItems[i]} - quantity: {amount} price: {0} UAH");
+                outOfStockExists = true;
+            }
+        }
+
+        if (!outOfStockExists)
+        {
+            Console.WriteLine("No items are out of stock.");
+        }
+    }
+}
